5a6bbd2df78f492f067b31b51a208f3cce9f5432
Sebastian Mueller <mueller@vsp.tu-berlin.de>
2020-02-11 14:50:29 +0100

    add local, regional and long distance train modes

--- src/main/java/org/matsim/run/MyMainModeIdentifier.java
+++ src/main/java/org/matsim/run/MyMainModeIdentifier.java
@@ -56,8 +56,13 @@ public final class MyMainModeIdentifier implements AnalysisMainModeIdentifier {
 		modeHierarchy.add( TransportMode.pt ) ;
 		modeHierarchy.add( TransportMode.ride ) ;
 		modeHierarchy.add( TransportMode.car ) ;
-		modeHierarchy.add( TransportMode.train ) ;
+		modeHierarchy.add( "localPublicTransport" ) ;
+		modeHierarchy.add( "regionalTrain" ) ;
+		modeHierarchy.add( "longDistanceTrain" ) ;
+//		modeHierarchy.add( TransportMode.train ) ;
 		modeHierarchy.add( TransportMode.airplane ) ;
+
+		
 		
 		// NOTE: This hierarchical stuff is not so great: is park-n-ride a car trip or a pt trip?  Could weigh it by distance, or by time spent
 		// in respective mode.  Or have combined modes as separate modes.  In any case, can't do it at the leg level, since it does not
